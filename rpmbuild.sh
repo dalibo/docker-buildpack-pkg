9b0986d9352dcac8b0722f36ed3244a576f50c2c
Étienne BERSAC <etienne.bersac@dalibo.com>
2022-12-09 18:16:59 +0100

    Add entrypoint for rpmbuild

---
+++ rpmbuild.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Generic script to build a RPM from spec and sources.
+#
+# Usage: rpmbuild.sh SPEC [SOURCE ...]
+#
+# Runs rpmbuild as testuser.
+
+set -eux
+
+BUILDDIR="${BUILDDIR-_build}"
+SPEC="$1"; shift
+SOURCES=("$@")
+specname="${SPEC##*/}"
+basespec="${specname%.spec}"
+
+# TOOLS
+
+retry yum-builddep -y "$SPEC"
+
+# SOURCES
+
+topdir=~testuser/rpmbuild
+mkdir -p "$topdir/SOURCES" "$topdir/SPECS"
+cp -vf "$SPEC" "$topdir/SPECS/"
+cp -rvf "${SOURCES[@]}" "$topdir/SOURCES/"
+# rpmbuild exige la propriété des fichiers.
+chown -R testuser "$topdir"
+
+# BUILD
+
+sudo -u testuser rpmbuild \
+    --clean \
+    --define "_topdir $topdir" \
+    -bb "$SPEC"
+
+# VERIFY
+
+rpms=( "$topdir"/RPMS/*/*.rpm )
+retry yum install -y "${rpms[@]}"
+for rpm in "${rpms[@]}" ; do
+    rpm -vv --verify -p "${rpm}"
+done
+
+# EXPORT
+
+ownership="$(stat -c %u:%g "$SPEC")"
+destdir="$(rpm --eval "$BUILDDIR/rpms%{rhel}/$basespec/")"
+mkdir -p "$destdir"
+chown -v "$ownership" "${destdir%/*/*}" "${destdir%/*}"
+cp -avf "${rpms[@]}" "$destdir"
+chown -Rv "$ownership" "$destdir"
+find "$destdir" -type f -printf "%P\n"
